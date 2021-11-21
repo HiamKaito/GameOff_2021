7a5d28b95821ba574715e9c46dab08ac7bdb987e
Chung <52872325+HiamKaito@users.noreply.github.com>
2021-11-21 19:54:28 +0700

    Base

---
+++ GameJame_Bug/Assets/Scripts/ImageEvent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ImageEvent : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+}
